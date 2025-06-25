7d2d2c38d739ae4a4d14bf0dd239d180def39f62
Sunil Singh <sunilsingh81188@gmail.com>
2025-06-26 00:11:13 +0530

    added Url redirecting functionality

--- url-shortner/src/main/java/com/url/shortner/service/UrlMappingService.java
+++ url-shortner/src/main/java/com/url/shortner/service/UrlMappingService.java
@@ -85,4 +85,19 @@ public class UrlMappingService {
         List<ClickEvent> clickEvents = clickEventRepository.findByUrlMappingInAndClickDateBetween(urlMappings, start.atStartOfDay(), end.plusDays(1).atStartOfDay());
         return clickEvents.stream().collect(Collectors.groupingBy(click-> click.getClickDate().toLocalDate(), Collectors.counting()));
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if (urlMapping != null) {
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+            // Record the click event
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+        return urlMapping;
+
+    }
 }
